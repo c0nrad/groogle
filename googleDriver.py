d268c16ee88f3929b7c2b2eddabd88a2ef172f0a
c0nrad <sclarsen@mtu.edu>
2013-01-29 19:51:22 -0500

    Added googleDriver interface

--- googleDriver.py
+++ googleDriver.py
@@ -1,3 +1,24 @@
 # googleDriver.py
 #
 # An interface for using google to find information
+
+
+
+# googleSearch(string search):
+#
+# Takes the search string, and does a google search on it. Should
+# return a list of top level URL's(string) that are returned from google.
+NUMER_OF_URLS_RETURNED=10 # Return the top 10 hits from google
+
+def googleSearch(search):
+    continue
+
+
+# googleImageSearch(string search):
+#
+# Takes the search string, and does a google image search on it.
+# Should return the URLs for the top few images.
+NUMBER_OF_IMAGE_URLS_RETURNED=10
+
+def googleImageSearch(search):
+    continue
