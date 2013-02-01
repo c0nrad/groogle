4f5b8fd36839195e4207eb44fd3c9db570f3d179
Adam Funkenbusch <Adam@wads-2-227-121.resnet.mtu.edu>
2013-01-31 21:12:38 -0500

    Added code to grab links

--- googleDriver.py
+++ googleDriver.py
@@ -2,6 +2,10 @@
 #
 # An interface for using google to find information
 
+import mechanize
+import re
+from bs4 import BeautifulSoup
+import html5lib
 
 
 # googleSearch(string search):
@@ -11,7 +15,22 @@
 NUMBER_OF_URLS_RETURNED=10 # Return the top 10 hits from google
 
 def googleSearch(search):
-    continue
+    # Convert search term into valid URL
+    search = search.replace(" ", "+")
+    url = "http://www.google.com/search?q=" + search
+
+    br = mechanize.Browser()
+
+    # Pretend to not be a robot  TODO: Ewwwwwwwwwwww
+    br.set_handle_robots(False)
+    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.01) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+
+    html = br.open(url).read()
+    soup = BeautifulSoup(html, "html5lib")
+
+    for link in soup.findAll('a', attrs={'href': re.compile("^/url\?q=")}):
+        # parse out the actual URL
+        continue
 
 
 # googleImageSearch(string search):
@@ -21,4 +40,9 @@ def googleSearch(search):
 NUMBER_OF_IMAGE_URLS_RETURNED=10
 
 def googleImageSearch(search):
-    continue
+    pass 
+
+
+if __name__ == "__main__":
+    print googleSearch("Adam Funkenbusch")
+
